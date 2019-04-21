777a6c7e5f0f7f5236648b17c92a8e253cf5a1e1
Clement Lakhal <clementlakhal@gmail.com>
2019-04-21 17:38:57 +0200

    Add config file to support multilanguages

---
+++ src/languages/i18n.js
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translations: {
+          hello: 'Hello',
+        },
+      },
+      fr: {
+        translations: {
+          hello: 'Bonjour',
+        },
+      },
+    },
+    fallbackLng: 'en',
+    debug: true,
+    ns: ['translations'],
+    defaultNS: 'translations',
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ',',
+    },
+    react: {
+      wait: true,
+    },
+  });
+
+export default i18n;

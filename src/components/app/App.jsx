abf4bbc6113188a1e73216ad462b384666e4ca79
Clement Lakhal <clementlakhal@gmail.com>
2019-04-21 19:27:05 +0200

    Update router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,8 +5,15 @@ import {
   Switch,
 } from 'react-router-dom';
 
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+
 import Header from '../header';
 import Footer from '../footer';
+import Sidebar from '../sidebar';
 
 import Home from '../home';
 import Faq from '../faq';
@@ -14,30 +21,38 @@ import Contact from '../contact/contact';
 import identity from '../identity';
 import About from '../about';
 import NoMatch from '../noMatch';
-import ManageDapps from '../manageDapps';
+import Manage from '../manage';
 import Privacy from '../privacy';
 import Terms from '../terms';
 import Wallet from '../wallet';
 import Submit from '../submit';
 
-
 const App = () => (
   <BrowserRouter>
     <div>
       <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/faq" component={Faq} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/identity" component={identity} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/manageDapps" component={ManageDapps} />
-        <Route exact path="/privacy" component={Privacy} />
-        <Route exact path="/terms" component={Terms} />
-        <Route exact path="/wallet" component={Wallet} />
-        <Route exact path="/submit" component={Submit} />
-        <Route component={NoMatch} />
-      </Switch>
+      <Container fluid>
+        <Row>
+          <Col xs="12" sm="3">
+            <Sidebar />
+          </Col>
+          <Col xs="12" sm="9">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/faq" component={Faq} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/identity" component={identity} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/manageDapps" component={Manage} />
+              <Route exact path="/privacy" component={Privacy} />
+              <Route exact path="/terms" component={Terms} />
+              <Route exact path="/wallet" component={Wallet} />
+              <Route exact path="/submit" component={Submit} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   </BrowserRouter>

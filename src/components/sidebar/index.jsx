112926bce1ff782d2958896659e7e68345874f12
Clement Lakhal <clementlakhal@gmail.com>
2019-04-21 19:27:55 +0200

    Update sidebar component

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import {
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import {
+  NavLink as RouterNavLink,
+} from 'react-router-dom';
+
+import CategoriesMenu from './categoriesMenu';
 
 const Sidebar = () => (
-  <h1>Sidebar</h1>
+  <div>
+    <Nav vertical>
+      <NavItem>
+        <NavLink tag={RouterNavLink} exact to="/wallet" activeClassName="active">dApp Store Wallet</NavLink>
+        <NavLink tag={RouterNavLink} exact to="/manage" activeClassName="active">dapps You Added</NavLink>
+        <NavLink tag={RouterNavLink} exact to="/identity" activeClassName="active">Manage Your Identity (EIN)</NavLink>
+        <NavLink tag={RouterNavLink} exact to="/submit" activeClassName="active">Submit your dApp</NavLink>
+      </NavItem>
+    </Nav>
+    <CategoriesMenu />
+  </div>
 );
 
 export default Sidebar;

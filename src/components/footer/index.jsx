2b063f0085d86a7cf9070ea85b685cd7df8458f8
Clement Lakhal <clementlakhal@gmail.com>
2019-04-22 17:28:53 +0200

    Update footer component

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import {
+  NavLink as RouterNavLink,
+} from 'react-router-dom';
 
 const Footer = () => (
-  <h1>Footer</h1>
+  <Container fluid className="py-3">
+    <Row>
+      <Col xs="12" sm="3" className="text-left">
+        Powered by Hydro
+      </Col>
+      <Col xs="12" sm="6" className="text-center">
+        <Nav fill>
+          <NavItem>
+            <NavLink tag={RouterNavLink} exact to="/privacy" activeClassName="active">Privacy</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RouterNavLink} exact to="/terms" activeClassName="active">Terms</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RouterNavLink} exact to="/about" activeClassName="active">About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RouterNavLink} exact to="/contact" activeClassName="active">Contact</NavLink>
+          </NavItem>
+        </Nav>
+      </Col>
+      <Col xs="12" sm="3" className="text-right">
+        Github
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Footer;

dd59ca1709c18eeab6887af531d680de13fba0ea
Clement Lakhal <clementlakhal@gmail.com>
2019-04-23 18:49:13 +0200

    Prevent users from accessing this page without a web3 provider

--- src/components/identity/index.jsx
+++ src/components/identity/index.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {
+  useWeb3Context,
+} from 'web3-react';
 
-const Identity = () => (
-  <h1>Manage your identity</h1>
-);
+import ProviderErrorHandler from '../web3Wrapper/providerErrorHandler';
+
+const Identity = () => {
+  const web3 = useWeb3Context();
+
+  if (web3.error) {
+    return (
+      <ProviderErrorHandler />
+    );
+  }
+
+  return (
+    <h1>Manage your identity</h1>
+  );
+};
 
 export default Identity;

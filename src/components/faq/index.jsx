ca9ec0986d89a93c997db0fa462c7dd499c7de07
Clement Lakhal <clementlakhal@gmail.com>
2019-04-23 11:53:35 +0200

    Add simple i18n test (to be deleted)

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const Faq = () => (
-  <h1>Faq</h1>
-);
+const Faq = () => {
+  const {
+    t,
+  } = useTranslation();
+
+  return (
+    <div>
+      <h1>Faq</h1>
+      <p>
+        {t('faqBody')}
+      </p>
+    </div>
+  );
+};
+
 
 export default Faq;

c869a455766f531d5062a38abb9569465fd19917
Clement Lakhal <clementlakhal@gmail.com>
2019-04-25 14:12:07 +0200

    Add wip homepage component

---
+++ src/components/home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import OnBoarding from '../onBoarding';
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <OnBoarding step="welcome" isOpen />
+  </div>
+);
+
+export default Home;

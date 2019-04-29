e79b36ff2a655cf0facf443f0e93cb9d962a70c9
Clement Lakhal <clementlakhal@gmail.com>
2019-04-29 13:56:49 +0200

    Add tabs to the homepage

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+import Featured from '../featured';
+
+function Home() {
+  const [tab, setTab] = useState('home');
+
+  return (
+    <div>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            onClick={() => setTab('home')}
+          >
+            Home
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            onClick={() => setTab('featured')}
+          >
+            Featured
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            onClick={() => setTab('popular')}
+          >
+            Most popular
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            onClick={() => setTab('last')}
+          >
+            Last Releases
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={tab}>
+        <TabPane tabId="home">
+          <p>
+            Home
+          </p>
+        </TabPane>
+        <TabPane tabId="featured">
+          <Featured />
+        </TabPane>
+        <TabPane tabId="popular">
+          <p>Popular dapps</p>
+        </TabPane>
+        <TabPane tabId="last">
+          <p>Last releases</p>
+        </TabPane>
+      </TabContent>
+    </div>
+  );
+}
 
 export default Home;

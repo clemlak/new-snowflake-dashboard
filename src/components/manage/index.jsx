022d24346db5fbdf599118d8b78ee0c41b7c9979
Clement Lakhal <clementlakhal@gmail.com>
2019-04-30 13:21:16 +0200

    Update Manage component

--- src/components/manage/index.jsx
+++ src/components/manage/index.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+  Row,
+  Col,
+  CardDeck,
+} from 'reactstrap';
+import {
+  useWeb3Context,
+} from 'web3-react';
 
-const Manage = () => (
-  <h1>Manage your dapps</h1>
-);
+import DappPreview from '../dappPreview';
+
+import {
+  getIdentity,
+} from '../../services/utilities';
+
+function Manage() {
+  const web3 = useWeb3Context();
+
+  const [resolvers, setResolvers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (web3.active && loading) {
+      getIdentity(web3.library, web3.account)
+        .then((identity) => {
+          setResolvers(identity.resolvers);
+          setLoading(false);
+        })
+        .catch((err) => { console.log(err); });
+    }
+  });
+
+  return (
+    <div>
+      <Row>
+        <Col>
+          <h1>
+            dApps You Added
+            {' '}
+            {resolvers.length}
+          </h1>
+          <p>View dApp Detail Page To Remove</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <CardDeck>
+            {resolvers.length > 0 ? (
+              <div>
+                {resolvers.map(resolver => (
+                  <DappPreview
+                    key={resolver}
+                    id={resolver}
+                  />
+                ))}
+              </div>
+            ) : (
+              <p>No dapps yet! :( </p>
+            )}
+          </CardDeck>
+        </Col>
+      </Row>
+    </div>
+  );
+}
 
 export default Manage;

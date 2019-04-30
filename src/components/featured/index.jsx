8f4ed6674918c7f293aa183d45b608fc6f038a7b
Clement Lakhal <clementlakhal@gmail.com>
2019-04-30 17:42:37 +0200

    Remove useless content inside component

--- src/components/featured/index.jsx
+++ src/components/featured/index.jsx
@@ -2,29 +2,9 @@ import React from 'react';
 import {
   Row,
   Col,
-  Button,
 } from 'reactstrap';
 
-import DappPreview from '../dappPreview';
-
-import configJson from '../../common/config/home.json';
-
 function Featured() {
-  const count = configJson.featured;
-
-  const cols = [];
-
-  for (let i = 0; i < count; i += 1) {
-    cols.push(
-      <Col key={i}>
-        <DappPreview
-          title="Dapp Raider"
-          subtitle="A cool game!"
-        />
-      </Col>,
-    );
-  }
-
   return (
     <div>
       <Row className="align-items-center">
@@ -34,14 +14,6 @@ function Featured() {
           </h1>
           <p>dApps Chosen Based on Demand</p>
         </Col>
-        <Col className="text-right">
-          <Button size="sm">
-            See more
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        {cols}
       </Row>
     </div>
   );

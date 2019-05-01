1252c8ea6f1a7f2211bf7d20d15fb91e4d7e1ea5
Clement Lakhal <clementlakhal@gmail.com>
2019-05-01 14:48:52 +0200

    Add component to display Legacy Dapps

---
+++ src/components/legacyDapp/index.jsx
@@ -0,0 +1,77 @@
+/**
+ * Display a legacy dapp in a modal
+ */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+} from 'reactstrap';
+import {
+  useWeb3Context,
+} from 'web3-react';
+
+import {
+  getAccountEin,
+} from '../../services/utilities';
+
+import {
+  Status,
+} from '../../legacy/Rinkeby/0x16fD6e2E1C4afB9C4e7B901141706596317e4ceB/index';
+
+function LegacyDapp(props) {
+  const web3 = useWeb3Context();
+
+  const [ein, setEin] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const {
+    id,
+    title,
+    isOpen,
+    toggle,
+  } = props;
+
+  if (web3.active && loading) {
+    getAccountEin(web3.library, web3.account)
+      .then((res) => {
+        if (res !== '') {
+          setEin(res);
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  if (!loading) {
+    return (
+      <Modal isOpen={isOpen} toggle={toggle} size="lg">
+        <ModalHeader toggle={toggle}>
+          {title}
+        </ModalHeader>
+        <ModalBody>
+          <Status ein={ein} />
+        </ModalBody>
+      </Modal>
+    );
+  }
+
+  return (
+    <p>
+      Loading...
+    </p>
+  );
+}
+
+LegacyDapp.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  toggle: PropTypes.func.isRequired,
+};
+
+export default LegacyDapp;

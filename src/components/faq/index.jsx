8014edab22219135a97e29a0d8ce5ecbce5a0073
Clement Lakhal <clementlakhal@gmail.com>
2019-05-04 20:04:00 +0200

    Update Faq component

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import {
+  Row,
+  Col,
+} from 'reactstrap';
+
+import QA from './qa';
+
+import faqJson from '../../common/config/faq.json';
 
 const Faq = () => {
   const {
@@ -8,10 +16,29 @@ const Faq = () => {
 
   return (
     <div>
-      <h1>Faq</h1>
-      <p>
-        {t('faqBody')}
-      </p>
+      <Row>
+        <Col>
+          <h1>Faq</h1>
+          <p>
+            {t('faqBody')}
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="accordion">
+            {faqJson.map(qa => (
+              <QA
+                question={qa.question}
+                answer={qa.answer}
+              />
+            ))}
+
+          </div>
+
+        </Col>
+      </Row>
+
     </div>
   );
 };

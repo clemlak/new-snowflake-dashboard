9643275829d0c42afca102b69a695558d3bea2b0
Clement Lakhal <clementlakhal@gmail.com>
2019-05-07 11:21:15 +0200

    Add Withdraw component

---
+++ src/components/wallet/depositWithdraw/withdraw.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Row,
+  Col,
+  Button,
+  Input,
+  FormGroup,
+  FormText,
+} from 'reactstrap';
+import {
+  IoIosArrowRoundForward,
+} from 'react-icons/io';
+import {
+  useWeb3Context,
+} from 'web3-react';
+
+import {
+  withdrawSnowflakeBalance,
+} from '../../../services/utilities';
+
+import TransactionButton from '../../transactionButton';
+
+function Withdraw(props) {
+  const {
+    user,
+    balance,
+    cancel,
+  } = props;
+
+  const [amount, setAmount] = useState(0);
+
+  const web3 = useWeb3Context();
+
+  return (
+    <div>
+      <Row className="mx-4 justify-content-center align-items-center no-gutters">
+        <Col sm="5">
+          <p className="dw__subtitle">
+            From
+          </p>
+        </Col>
+        <Col sm="2" />
+        <Col sm="5">
+          <p className="dw__subtitle">
+            To
+          </p>
+        </Col>
+      </Row>
+      <Row className="mx-4 justify-content-center align-items-center no-gutters">
+        <Col sm="5">
+          <div className="dw__from">
+            <FormGroup className="dw__form-group">
+              <Input
+                type="number"
+                className="dw__input"
+                placeholder="0"
+                onChange={e => setAmount(e.target.value)}
+              />
+              <FormText
+                className="dw__form-text"
+              >
+                dApp Store Wallet
+              </FormText>
+            </FormGroup>
+          </div>
+        </Col>
+        <Col sm="2" className="text-center">
+          <IoIosArrowRoundForward className="dw__arrow" />
+        </Col>
+        <Col sm="5">
+          <div className="dw__to">
+            <p className="dw__balance">
+              {0}
+            </p>
+            <p className="dw__to-small-text">
+              {user}
+            </p>
+          </div>
+        </Col>
+      </Row>
+      <Row className="pt-5 mx-4 justify-content-center align-items-center no-gutters">
+        <Col className="text-left">
+          <Button onClick={cancel}>
+            Cancel
+          </Button>
+        </Col>
+        <Col className="text-right">
+          <TransactionButton
+            color="success"
+            text="Confirm"
+            send={() => withdrawSnowflakeBalance(web3.library, web3.account, amount)}
+            finalAction={cancel}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+Withdraw.propTypes = {
+  balance: PropTypes.string.isRequired,
+  cancel: PropTypes.func.isRequired,
+  user: PropTypes.string.isRequired,
+};
+
+export default Withdraw;

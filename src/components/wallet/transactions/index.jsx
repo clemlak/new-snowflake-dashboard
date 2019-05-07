6c38363e7e6884339ed6452ebffd98d6351cdd75
Clement Lakhal <clementlakhal@gmail.com>
2019-05-07 14:27:39 +0200

    Update component to display transactions

--- src/components/wallet/transactions/index.jsx
+++ src/components/wallet/transactions/index.jsx
@@ -1,6 +1,6 @@
 /**
  * Displays the transactions linked to the current account
- * TODO: Fetch the transactions
+ * TODO: It is not clear if the "purchases" must be linked to Hydro tokens or dApps?
  */
 
 import React, { useState } from 'react';
@@ -20,6 +20,7 @@ import {
 import {
   getPastDeposits,
   getPastPurchases,
+  getPastWithdrawals,
 } from '../../../services/utilities';
 
 import Transaction from './transaction';
@@ -27,7 +28,30 @@ import Transaction from './transaction';
 function Transactions() {
   const [tab, setTab] = useState('all');
   const [deposits, setDeposits] = useState([]);
+  const [withdrawals, setWithdrawals] = useState([]);
   const [purchases, setPurchases] = useState([]);
+
+  const web3 = useWeb3Context();
+
+  if (web3.active && deposits.length === 0 && withdrawals.length === 0 && purchases.length === 0) {
+    getPastDeposits(web3.library, web3.account)
+      .then((res) => {
+        setDeposits(res);
+
+        return getPastWithdrawals(web3.library, web3.account);
+      })
+      .then((res) => {
+        setWithdrawals(res);
+
+        return getPastPurchases(web3.library, web3.account);
+      })
+      .then((res) => {
+        setPurchases(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <div>
@@ -85,20 +109,44 @@ function Transactions() {
           </Nav>
           <TabContent activeTab={tab}>
             <TabPane tabId="all">
-              <Transaction
-                type="withdraw"
-                date={Date.now()}
-                amount="4200"
-              />
+              {purchases.concat(deposits.concat(withdrawals)).map(tx => (
+                <Transaction
+                  key={tx.txHash}
+                  type={tx.event}
+                  date={Date.now()}
+                  amount={tx.amount}
+                />
+              ))}
             </TabPane>
             <TabPane tabId="deposits">
-              Deposits
+              {deposits.map(deposit => (
+                <Transaction
+                  key={deposit.txHash}
+                  type={deposit.event}
+                  date={Date.now()}
+                  amount={deposit.amount}
+                />
+              ))}
             </TabPane>
             <TabPane tabId="withdraws">
-              Withdraws
+              {withdrawals.map(withdrawal => (
+                <Transaction
+                  key={withdrawal.txHash}
+                  type={withdrawal.event}
+                  date={Date.now()}
+                  amount={withdrawal.amount}
+                />
+              ))}
             </TabPane>
             <TabPane tabId="purchases">
-              Purchases
+              {purchases.map(purchase => (
+                <Transaction
+                  key={purchase.txHash}
+                  type={purchase.event}
+                  date={Date.now()}
+                  amount={purchase.withdrawAllowance}
+                />
+              ))}
             </TabPane>
           </TabContent>
         </Col>

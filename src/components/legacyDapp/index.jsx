82d10b41e892f139baaf5859cbf48cc4324275d1
Clement Lakhal <clementlakhal@gmail.com>
2019-05-11 16:46:35 +0200

    Add comment

--- src/components/legacyDapp/index.jsx
+++ src/components/legacyDapp/index.jsx
@@ -1,5 +1,8 @@
 /**
- * Display a legacy dapp in a modal
+ * Displays a legacy dapp in a modal
+ * NOTE: Legacy dapps were created based on the previous front-end
+ * The goal is now to move dapps from the front-end to their own website
+ * Meanwhile, legacy dapps are still hosted here
  */
 
 import React, { useState } from 'react';

a031b0160cef7c5f94d90040b14945e58feb393e
Clement Lakhal <clementlakhal@gmail.com>
2019-05-17 12:01:21 +0200

    Add getBalanceUsd function

---
+++ src/services/hydroPrice.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+function getBalanceUsd(lib, balance) {
+  return axios.get('https://api.coingecko.com/api/v3/coins/hydro')
+    .then((res) => {
+      const currentPrice = res.data.market_data.current_price.usd;
+      const value = parseFloat(currentPrice) * parseFloat(balance);
+
+      return value;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export {
+  getBalanceUsd,
+};

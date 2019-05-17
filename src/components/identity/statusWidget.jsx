fcdf75af7c8a9c273d34fb79f4f7a01c0cb12f11
Clement Lakhal <clementlakhal@gmail.com>
2019-05-17 11:00:24 +0200

    Add total integration of the Status dApp within the widget

--- src/components/identity/statusWidget.jsx
+++ src/components/identity/statusWidget.jsx
@@ -1,3 +1,8 @@
+/**
+ * Displays a widget connected to the Status dApp
+ * TODO: The Get / Edit bug is buggy, the center of the button doesn't seem clickable
+ */
+
 import React, { useState } from 'react';
 import {
   useWeb3Context,
@@ -8,21 +13,35 @@ import {
   Button,
 } from 'reactstrap';
 
-import DappPreview from '../dappPreview';
+import Purchase from '../purchase';
+import LegacyDapp from '../legacyDapp';
 
 import {
   getStatus,
+  isResolverFor,
 } from '../../services/utilities';
 
 function StatusWidget() {
   const [status, setStatus] = useState('');
+  const [isStatusAdded, setIsStatusAdded] = useState(false);
+  const [isAddedChecked, setIsAddedChecked] = useState(false);
 
+  const [isPurchaseModalOpen, setIsPurchaseModalOpen] = useState(false);
+  const [isDappModalOpen, setIsDappModalOpen] = useState(false);
+
+  const statusAddress = '0x16fD6e2E1C4afB9C4e7B901141706596317e4ceB';
   const web3 = useWeb3Context();
 
-  if (web3.active) {
+  if (web3.active && !isAddedChecked) {
     getStatus(web3.library, web3.account)
       .then((res) => {
         setStatus(res);
+
+        return isResolverFor(web3.library, web3.account, statusAddress);
+      })
+      .then((res) => {
+        setIsStatusAdded(res);
+        setIsAddedChecked(true);
       })
       .catch((err) => {
         console.log(err);
@@ -30,29 +49,51 @@ function StatusWidget() {
   }
 
   return (
-    <Row className="mb-4 no-gutters status">
-      <Col>
-        <Row>
-          <Col>
-            <p className="status__title">
-              My Status:
-            </p>
-          </Col>
-          <Col>
-            <Button className="status__edit">
-              Edit
-            </Button>
-          </Col>
-        </Row>
-        <p className="status__current">
-          {status !== '' ? (
-            status
-          ) : (
-            'No status yet!'
-          )}
-        </p>
-      </Col>
-    </Row>
+    <div>
+      <LegacyDapp
+        id={statusAddress}
+        title="Status"
+        isOpen={isDappModalOpen}
+        toggle={() => setIsDappModalOpen(false)}
+      />
+      <Purchase
+        id={statusAddress}
+        title="Status"
+        isOpen={isPurchaseModalOpen}
+        toggle={() => setIsPurchaseModalOpen(false)}
+      />
+      <Row className="mb-4 no-gutters status">
+        <Col>
+          <Row>
+            <Col>
+              <p className="status__title">
+                My Status:
+              </p>
+            </Col>
+            <Col>
+              {isStatusAdded ? (
+                <Button className="status__edit" onClick={() => setIsDappModalOpen(true)}>
+                  Edit
+                </Button>
+              ) : (
+                <Button className="status__edit" onClick={() => setIsPurchaseModalOpen(true)}>
+                  Get
+                </Button>
+              )}
+
+            </Col>
+          </Row>
+          <p className="status__current">
+            {status !== '' ? (
+              status
+            ) : (
+              'No status yet!'
+            )}
+          </p>
+        </Col>
+      </Row>
+    </div>
+
   );
 }
 

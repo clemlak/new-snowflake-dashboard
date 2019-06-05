f453210860bff580d3829fac745f8fba3c321fe7
Clement Lakhal <clementlakhal@gmail.com>
2019-06-05 16:01:09 +0200

    Remove useless name checking (homepage is already a fallback page)

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -15,35 +15,29 @@ function Category({
   const { name } = match.params;
 
   return (
-    <>
     <div>
-      {match.params.name ? (
+      <div>
         <h1 className="title">
           {`${name} dApps`}
         </h1>
-      ) : (
-        <p>
-          No param in query string, displaying default category
-        </p>
-      )}
-    </div>
+      </div>
 
-    <Row className="mt-5">
-      <Col>
-        <Row className="additional-help align-items-center">
-          <Col>
-            <h4 className="additional-help__title">Be The First</h4>
-            <p className="additional-help__subtitle">Submit your dApp to this category and earn a Hydro bounty!</p>
-          </Col>
-          <Col sm="4" className="text-right">
-            <Button className="btn-outlined">
-              Submit dApp
-            </Button>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-    </>
+      <Row className="mt-5">
+        <Col>
+          <Row className="additional-help align-items-center">
+            <Col>
+              <h4 className="additional-help__title">Be The First</h4>
+              <p className="additional-help__subtitle">Submit your dApp to this category and earn a Hydro bounty!</p>
+            </Col>
+            <Col sm="4" className="text-right">
+              <Button className="btn-outlined">
+                Submit dApp
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 }
 

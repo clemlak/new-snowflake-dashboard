b9c4f1478e081aa4548de384a8466d275a522359
Clement Lakhal <clementlakhal@gmail.com>
2019-06-20 17:34:41 +0200

    Install storybook-react-router package

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 
 const req = require.context('../src', true, /\.stories\.jsx$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(StoryRouter());
 
 configure(loadStories, module);

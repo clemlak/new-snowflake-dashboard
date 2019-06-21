13517fb069eff156307ed07633a85b660104656d
Clement Lakhal <clementlakhal@gmail.com>
2019-06-21 17:13:06 +0200

    Update Storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 import { configure, addDecorator } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 
+import '../src/common/style/index.scss';
+
 const req = require.context('../src', true, /\.stories\.jsx$/);
 
 function loadStories() {

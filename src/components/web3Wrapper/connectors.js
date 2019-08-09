416dfb3eb0c6c6130a466e52fab7beaa0d962ce3
Clement Lakhal <clementlakhal@gmail.com>
2019-08-09 16:10:20 +0200

    Update connector

--- src/components/web3Wrapper/connectors.js
+++ src/components/web3Wrapper/connectors.js
@@ -6,7 +6,7 @@ import BitskiConnector from './bitskiConnector';
 
 const { InjectedConnector } = Connectors;
 
-const MetaMask = new InjectedConnector({ supportedNetworks: [4] });
+const MetaMask = new InjectedConnector();
 
 const Bitski = new BitskiConnector('590d77f9-f1c9-4b4d-aa4e-20ad706a0861');
 
